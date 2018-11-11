a2db7529bc7f250f7cbf68b552b64c696f37b8c1
DONGKEON KIM <amos@shallwefootball.com>
2018-11-11 17:23:30 +0900

    add spec with enzyme

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { from } from "rxjs";
-import { compose, setObservableConfig } from "recompose";
-import { duration$, goal } from "./utils";
-import eases from "eases";
+import { setObservableConfig } from "recompose";
+import App from "./App";
 
 setObservableConfig({
   fromESObservable: from
 });
 
-const App = () => {
-  return (
-    <div>
-      <style jsx>{``}</style>
-      <h1>We Could be</h1>
-    </div>
-  );
-};
-
-const _App = compose()(App);
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<_App />, rootElement);
+ReactDOM.render(<App />, rootElement);
